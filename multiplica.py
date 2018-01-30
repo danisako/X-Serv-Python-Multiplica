33f424f3562f45a53b7b8a9333edf381efe4e085
Daniel Suarez <dsuarezm@gamma28.aulas.gsyc.urjc.es>
2018-01-30 10:48:02 +0100

    tablas multiplicar

---
+++ multiplica.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+for num in list(range(1,11)):
+	print('TABLA DEL ',num, '\n')
+	for num2 in list(range(1,11)):
+		
+		print(num ,'por',num2, 'es', num*num2)
+		
+
+#print(num,"por",num2, num*num2)
